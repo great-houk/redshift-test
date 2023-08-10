11020e2e55bacc33cb85df6dab0479e0f0eaf03e
Tyler Richard <tylerrichard215@gmail.com>
2023-08-09 20:25:14 -0400

    Working Serial

--- src/main.rs
+++ src/main.rs
@@ -1,180 +1,129 @@
-#![no_std]
 #![no_main]
-use core::fmt::Write;
+#![no_std]
+#![feature(type_alias_impl_trait)]
+
 use cortex_m_rt as _;
-use delay::Delay;
-use embedded_hal::{digital::v2::InputPin, timer::CountDown};
-use embedded_hal_alpha::delay::DelayUs;
-use embedded_hal_bus::spi::ExclusiveDevice;
-use gpio::Pin;
-use hal::{drivers::Timer, time::*, traits::wg::spi::MODE_3, Pins, UsbBus};
-use heapless::String;
-use lpc55_hal as hal;
-use nb::block;
 use panic_rtt_target as _;
-use paw3399::{registers as regs, MotionRead, Paw3399};
-#[allow(unused)]
-use rtt_target::{rdbg as dbg, rprint as print, rprintln as println};
-use spi::SpiMaster;
-use usb_device::{
-    device::{UsbDeviceBuilder, UsbVidPid},
-    prelude::UsbDeviceState,
-    UsbError,
-};
-use usbd_hid::{
-    descriptor::{MouseReport, SerializedDescriptor},
-    hid_class::HIDClass,
-};
 
-mod delay;
-mod gpio;
-mod setup;
-mod spi;
-
-fn run() -> ! {
-    let hal = hal::new();
-
-    let mut anactrl = hal.anactrl;
-    let mut syscon = hal.syscon;
-    let mut pmc = hal.pmc;
-    let mut iocon = hal.iocon.enabled(&mut syscon);
-    let mut gpio = hal.gpio.enabled(&mut syscon);
-    let pins = Pins::take().unwrap();
-
-    let clocks = hal::ClockRequirements::default()
-        .system_frequency(150.MHz())
-        .configure(&mut anactrl, &mut pmc, &mut syscon)
-        .expect("Clock configuration failed");
-
-    let spi_device = {
-        let spi = hal
-            .flexcomm
-            .6
-            .enabled_as_spi(&mut syscon, &clocks.support_flexcomm_token().unwrap());
-        let sck = pins.pio1_12.into_spi6_sck_pin(&mut iocon);
-        let mosi = pins.pio1_13.into_spi6_mosi_pin(&mut iocon);
-        let miso = pins.pio1_16.into_spi6_miso_pin(&mut iocon);
-        let speed: Hertz = 10.MHz().try_into().unwrap();
-        let spi = SpiMaster::new(spi, (sck, mosi, miso), speed, MODE_3);
-
-        let cs: Pin<_, _> = pins
-            .pio0_15
-            .into_gpio_pin(&mut iocon, &mut gpio)
-            .into_output_high()
-            .into();
-        let delay_timer_spi: Delay<_> = Timer::new(
-            hal.ctimer
-                .0
-                .enabled(&mut syscon, clocks.support_1mhz_fro_token().unwrap()),
-        )
-        .into();
-        ExclusiveDevice::new(spi, cs, delay_timer_spi)
+#[rtic::app(device = lpc55_hal::raw, dispatchers = [FLEXCOMM0])]
+mod app {
+    use core::fmt::Write;
+    use heapless::String;
+    use lpc55_hal::{
+        self as hal,
+        drivers::Timer,
+        time::{DurationExtensions, RateExtensions},
+        Pins,
     };
+    use lpc55_usbhs::{UsbHS, UsbHSBus};
+    use rtt_target::{rdbg as dbg, rprint as print, rprintln as println, rtt_init_default};
+    use usb_device::{class_prelude::UsbBusAllocator, prelude::*};
+    use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
-    let mut sensor = {
-        let delay_timer_sensor: Delay<_> = Timer::new(
-            hal.ctimer
-                .1
-                .enabled(&mut syscon, clocks.support_1mhz_fro_token().unwrap()),
-        )
-        .into();
-        let reset: Pin<_, _> = pins
-            .pio1_8
-            .into_gpio_pin(&mut iocon, &mut gpio)
-            .into_output_low()
-            .into();
-        // SAFTEY: SPI is in mode 3
-        unsafe { Paw3399::new(spi_device, delay_timer_sensor, reset) }.unwrap()
-    };
-
-    let motion_pin = pins
-        .pio0_19
-        .into_gpio_pin(&mut iocon, &mut gpio)
-        .into_input();
-
-    // Check sensor status
-    let mut delay: Delay<_> = Timer::new(
-        hal.ctimer
-            .3
-            .enabled(&mut syscon, clocks.support_1mhz_fro_token().unwrap()),
-    )
-    .into();
-    sensor.write(regs::Observation, 0x00).unwrap();
-    delay.delay_ms(600);
-    let res = sensor.read(regs::Observation).unwrap();
-    if res == 0xB7 || res == 0xBF {
-        println!("Sensor Initialized Successfully!");
-    } else {
-        panic!("Sensor failed to start correctly...");
+    #[shared]
+    struct Shared {
+        serial: SerialPort<'static, UsbHSBus>,
+        device: UsbDevice<'static, UsbHSBus>,
     }
 
-    let usb_hs = {
-        let mut delay_timer_usb = Timer::new(
-            hal.ctimer
-                .2
-                .enabled(&mut syscon, clocks.support_1mhz_fro_token().unwrap()),
-        );
-        hal.usbhs.enabled_as_device(
-            &mut anactrl,
-            &mut pmc,
-            &mut syscon,
-            &mut delay_timer_usb,
-            clocks.support_usbhs_token().unwrap(),
-        )
-    };
+    #[local]
+    struct Local {}
 
-    let usb_bus = UsbBus::new(usb_hs, pins.pio0_22.into_usb0_vbus_pin(&mut iocon));
+    #[init(local = [usb_bus: Option<UsbBusAllocator<UsbHSBus>> = None])]
+    fn init(cx: init::Context) -> (Shared, Local) {
+        let rtt = rtt_init_default!();
+        rtt_target::set_print_channel(rtt.up.0);
+        // Saftey Delay, so it's possible to halt before user code runs
+        cortex_m::asm::delay(10_000_000);
+        println!("Start up finished!");
 
-    let mut hid = HIDClass::new(&usb_bus, MouseReport::desc(), 1);
+        let hal = hal::from((cx.device, cx.core.into()));
 
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0xcc1d))
-        .manufacturer("Tyler")
-        .product("Test Serial! 🌈")
-        .serial_number("2023-07-05")
-        .device_release(0xBEEF)
-        // Must be 64 bytes for HighSpeed
-        .max_packet_size_0(64)
-        .build();
+        let mut anactrl = hal.anactrl;
+        let mut syscon = hal.syscon;
+        let mut pmc = hal.pmc;
+        let mut iocon = hal.iocon.enabled(&mut syscon);
+        let mut gpio = hal.gpio.enabled(&mut syscon);
+        let pins = Pins::take().unwrap();
 
-    let mut delx = 0;
-    let mut dely = 0;
+        let clocks = hal::ClockRequirements::default()
+            .system_frequency(150.MHz())
+            .configure(&mut anactrl, &mut pmc, &mut syscon)
+            .expect("Clock configuration failed");
 
-    loop {
-        if motion_pin.is_low().unwrap() {
-            let (delta_x, delta_y) = sensor.motion_read_delta_only().unwrap();
-            delx += delta_x;
-            dely += delta_y;
+        let usb_hs = {
+            let mut delay_timer_usb = Timer::new(
+                hal.ctimer
+                    .2
+                    .enabled(&mut syscon, clocks.support_1mhz_fro_token().unwrap()),
+            );
+            UsbHS::new(
+                hal.usbhs,
+                &mut syscon,
+                &mut pmc,
+                &mut anactrl,
+                &mut delay_timer_usb,
+            )
+        };
+
+        let bus = cx.local.usb_bus.insert(UsbHSBus::new(usb_hs));
+
+        let serial = SerialPort::new(bus);
+
+        let device = UsbDeviceBuilder::new(bus, UsbVidPid(0x1209, 0xcc1d))
+            .manufacturer("Tyler")
+            .product("Test Serial! 🌈")
+            .serial_number("2023-07-05")
+            .device_release(0xBEEF)
+            // Must be 64 bytes for HighSpeed
+            .device_class(USB_CLASS_CDC)
+            .max_packet_size_0(64)
+            .build();
+
+        (Shared { serial, device }, Local {})
+    }
+
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            cortex_m::asm::nop();
         }
+    }
 
-        if !usb_dev.poll(&mut [&mut hid]) {
-            continue;
-        }
+    #[task(binds = USB1, priority = 1, local = [i: u8 = 0], shared = [serial, device])]
+    fn usb(cx: usb::Context) {
+        let int: u32 = unsafe { core::ptr::read_volatile(0x4009_4020 as _) };
+        let cmd: u32 = unsafe { core::ptr::read_volatile(0x4009_4000 as _) };
+        println!("{} USB Int 0x{:X} 0x{:X}", cx.local.i, int, cmd);
+        *cx.local.i = u8::wrapping_add(*cx.local.i, 1);
 
-        let dx = trucate_i16(delx);
-        let dy = trucate_i16(dely);
-
-        match hid.push_input(&MouseReport {
-            x: -dx / 2, // Negative, for some reason
-            y: dy / 2,
-            buttons: 0,
-            wheel: 0,
-            pan: 0,
-        }) {
-            Ok(_) => {
-                delx -= dx as i16;
-                dely -= dy as i16;
+        let device = cx.shared.device;
+        let serial = cx.shared.serial;
+        (device, serial).lock(|device, serial| {
+            if device.poll(&mut [serial]) && device.state() == UsbDeviceState::Configured {
+                println!("Inner");
+                let _ = echo_serial::spawn();
             }
-            Err(_) => {}
-        }
+        });
     }
-}
 
-fn trucate_i16(num: i16) -> i8 {
-    if num > core::i8::MAX as i16 {
-        core::i8::MAX
-    } else if num < core::i8::MIN as i16 {
-        core::i8::MIN
-    } else {
-        num as i8
+    #[task(local = [buf: [u8; 256] = [0; 256], string_buf: String<256> = String::new()], shared = [serial, device], priority = 1)]
+    async fn echo_serial(mut cx: echo_serial::Context) {
+        println!("Echo!");
+
+        cx.shared
+            .serial
+            .lock(|serial| match serial.read(cx.local.buf) {
+                Ok(count) => {
+                    let _ = serial.write(&cx.local.buf[..count]);
+                }
+                Err(UsbError::WouldBlock) => {}
+                Err(err) => {
+                    let buf = cx.local.string_buf;
+                    buf.clear();
+                    write!(buf, "Usb Error: {err:?}").unwrap();
+                    let _ = serial.write(buf.as_bytes());
+                }
+            });
     }
 }
